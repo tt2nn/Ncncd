9fff0f1cba2760ba627ed388e8fcd16467222891
lihaotian <lihaotian@mirror-networks.com>
2017-11-21 17:05:25 +0800

    1.0.11

---
+++ NcncdFramework/model/common/CommonRequest.swift
@@ -0,0 +1,27 @@
+//
+//  CommonRequest.swift
+//  Ncncd
+//  通用请求实体类
+//  Created by lihaotian on 2017/11/9.
+//  Copyright © 2017年 miao. All rights reserved.
+//
+
+import UIKit
+
+class CommonRequest: NSObject {
+    
+    // AppKey
+    var appKey = ""
+    
+    // 时间戳
+    var timestamp = 0
+    
+    // 手机号
+    var telephone = ""
+    
+    // 数据
+    var data = ""
+    
+    // 签名
+    var sign = ""
+}
---
+++ NcncdFramework/model/common/CommonResponse.swift
@@ -0,0 +1,18 @@
+//
+//  CommonResponse.swift
+//  Ncncd
+//  通用响应实体类
+//  Created by lihaotian on 2017/11/9.
+//  Copyright © 2017年 miao. All rights reserved.
+//
+
+import UIKit
+
+class CommonResponse: NSObject {
+    
+    // 错误信息
+    var errMsg = ""
+    
+    // 错误编号
+    var errNo = 0
+}
